b1e1fc1989975f224548a43b1ffb637e6d357f95
Шкаринский Валерий <valerashk3@gmail.com>
2023-12-22 12:42:34 +0600

    qq

--- mainproject/mainproject/urls.py
+++ mainproject/mainproject/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('mainblog.urls')),
+    path('', include('mainblog.urls')),
     path('acc/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls'))
 
